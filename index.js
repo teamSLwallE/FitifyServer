bca6a0bcf8c58f3eae4b2602554bcf02723c6d64
teamSLwallE <118100389+teamSLwallE@users.noreply.github.com>
2022-11-13 13:58:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const connect = require("./src/Congif/db");
 const cors = require("cors");
-const PORT = process.env.PORT;
+
 
 const topSellingRoute = require("./src/features/topselling/topselling.route");
 const topArrivalRoute = require("./src/features/TopArrivals/toparrivals.route");
